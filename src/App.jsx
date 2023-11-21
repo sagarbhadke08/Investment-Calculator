543275d1b1058ca72f6c26901f528116e4beaa42
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-11-21 06:24:31 +0530

    Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentfier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentfier]: newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput}/>
+    </>
+  );
 }
 
-export default App
+export default App;
